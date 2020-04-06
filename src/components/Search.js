647ba14226d0ee58e607308c0fc790be394bbc21
Jose <pjose14.jp38@yahoo.com>
2020-04-05 23:52:07 -0500

    took clear button off needs work

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,7 @@ function Search(props) {
                     placeholder="Search country..."
                 />
                     <button className = 'searchButton' onClick = {props.onClick}>Search</button>
-                    <button className = 'clearButton' style = {{marginLeft: '10px'}} onClick = {props.onDoubleClick} variant="secondary">Clear</button>
+                    {/* <button className = 'clearButton' style = {{marginLeft: '10px'}} onClick = {props.onDoubleClick} variant="secondary">Clear</button> */}
 
             </div>
     )
